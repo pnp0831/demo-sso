d9daeb46cc369063e17ffac9087cc1d6bacba058
Phat Nguyen Pham <phpham@simpsonmfg.com>
2023-03-30 16:48:57 +0700

    .

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,43 +1,23 @@
-// Cài đặt next-auth và sử dụng strategy Google
-// npm install next-auth google
-// pages/api/auth/[...nextauth].js
 import NextAuth from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
 import CredentialsProvider from "next-auth/providers/credentials";
 
 export default NextAuth({
   providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    }),
     CredentialsProvider({
-      // The name to display on the sign in form (e.g. "Sign in with...")
       name: "Credentials",
-      // `credentials` is used to generate a form on the sign in page.
-      // You can specify which fields should be submitted, by adding keys to the `credentials` object.
-      // e.g. domain, username, password, 2FA token, etc.
-      // You can pass any HTML attribute to the <input> tag through the object.
       credentials: {
-        username: { label: "Username", type: "text", placeholder: "jsmith" },
+        email: { label: "Email", type: "text" },
         password: { label: "Password", type: "password" },
       },
-      async authorize(credentials, req) {
-        // Add logic here to look up the user from the credentials supplied
+      authorize: async (credentials) => {
+        console.log("credentials", credentials);
         const user = {
-          name: credentials?.username,
-          password: credentials?.password,
+          ...credentials,
+          name: credentials.username,
         };
-
-        if (user) {
-          // Any object returned will be saved in `user` property of the JWT
-          return user;
-        } else {
-          // If you return null then an error will be displayed advising the user to check their details.
-          return null;
-
-          // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
-        }
+        // Validate the credentials here
+        // and return the user object if they are valid
+        return user;
       },
     }),
   ],
@@ -49,7 +29,7 @@ export default NextAuth({
       return true;
     },
     async redirect({ url, baseUrl }) {
-      return url;
+      return baseUrl;
     },
   },
 });
