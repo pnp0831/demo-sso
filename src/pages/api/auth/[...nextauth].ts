43f210aa77682f8334c556230a2dbcd8dc5c5f63
Phat Nguyen Pham <phpham@simpsonmfg.com>
2023-03-31 14:48:22 +0700

    .

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,6 +1,10 @@
+// Cài đặt next-auth và sử dụng strategy Google
+// npm install next-auth google
+// pages/api/auth/[...nextauth].js
 import NextAuth from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 import GoogleProvider from "next-auth/providers/google";
+import WorkOSProvider from "next-auth/providers/workos";
 
 export default NextAuth({
   providers: [
@@ -8,42 +12,32 @@ export default NextAuth({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
-    // CredentialsProvider({
-    //   name: "Credentials",
-    //   id: process.env.NEXTAUTH_SECRET,
-    //   credentials: {
-    //     email: { label: "Email", type: "text" },
-    //     password: { label: "Password", type: "password" },
-    //   },
-    //   authorize: async (credentials) => {
-    //     console.log("credentials", credentials);
-    //     const user = {
-    //       ...credentials,
-    //       name: credentials.username,
-    //     };
-    //     // Validate the credentials here
-    //     // and return the user object if they are valid
-    //     return user;
-    //   },
-    // }),
   ],
-  cookie: {
-    domain: ".vercel.app",
-  },
   cookies: {
     domain: ".vercel.app",
   },
   callbacks: {
     async signIn({ user, account, profile }) {
-      console.log("user", { user, account, profile });
       return true;
     },
     async signOut(a) {
       return true;
     },
     async redirect({ url, baseUrl }) {
-      console.log({ url, baseUrl });
       return url;
+    },
+    async session({ session, token, user }) {
+      const a = await fetch(
+        `${process.env.NEXTAUTH_URL}/api/auth/session`
+      ).then((res) => res.json());
+
+      console.log("A", a);
+
+      // Send properties to the client, like an access_token and user id from a provider.
+      // session.accessToken = token.accessToken;
+      // session.user.id = token.id;
+
+      return a;
     },
   },
 });
