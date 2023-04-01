a2fb5629dc657a5b8f4eace7c6196adfc04f71f3
Phat Nguyen Pham <phpham@simpsonmfg.com>
2023-04-01 21:45:52 +0700

    .

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,9 +3,12 @@ import type { AppProps } from "next/app";
 import { SessionProvider } from "next-auth/react";
 import { useEffect } from "react";
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}) {
   return (
-    <SessionProvider>
+    <SessionProvider session={session}>
       <Component {...pageProps} />
     </SessionProvider>
   );
